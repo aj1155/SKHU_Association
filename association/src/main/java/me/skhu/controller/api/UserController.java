8448738f9e69c66083b242e2411eace8270ed98b
aj1155 <aj1155@naver.com>
2017-01-20 15:17:51 +0900

    [api] user 생성,조회 추가

--- association/src/main/java/me/skhu/controller/api/UserController.java
+++ association/src/main/java/me/skhu/controller/api/UserController.java
@@ -1,5 +1,6 @@
 package me.skhu.controller.api;
 
+import me.skhu.controller.model.request.UserRequest;
 import me.skhu.controller.model.response.AsctApiResponse;
 import me.skhu.controller.model.response.UserResponse;
 import me.skhu.service.JwtTokenService;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 /**
@@ -25,9 +27,45 @@ public class UserController {
     @Autowired
     private JwtTokenService jwtTokenService;
 
-    @RequestMapping(value = {"/list/category/{categoryId}"},method = RequestMethod.GET)
-    public AsctApiResponse<List<UserResponse>> readUserByCategoryId(@PathVariable String categoryId){
+    /****** create *****/
+    //Todo response로 Json util 형태로 보내주기
+    @RequestMapping(value = "/create",method = RequestMethod.POST)
+    public AsctApiResponse<UserResponse> create(@Valid UserRequest userRequest){
+        UserResponse user = userService.createUser(userRequest);
+        return new AsctApiResponse<>(user);
+    }
+
+    /****** read *****/
+
+    /* 카테고리별 user*/
+    @RequestMapping(value = "/list/category/{categoryId}",method = RequestMethod.GET)
+    public AsctApiResponse<List<UserResponse>> readUserByCategory(@PathVariable String categoryId){
         List<UserResponse> list = this.userService.readUserByCategoryId(Integer.parseInt(categoryId));
         return new AsctApiResponse<>(list);
     }
+
+    /* 기수별 user*/
+    @RequestMapping(value = "/list/category/{categoryId}/grade/{grade}",method = RequestMethod.GET)
+    public AsctApiResponse<List<UserResponse>> readUserByGrade(@PathVariable String categoryId,@PathVariable String grade){
+        List<UserResponse> list = this.userService.readUserByGrade(Integer.parseInt(categoryId),Integer.parseInt(grade));
+        return new AsctApiResponse<>(list);
+    }
+
+    /* 기수별 임원 */
+    @RequestMapping(value = "/manager/category/{categoryId}/grade/{grade}",method = RequestMethod.GET)
+    public AsctApiResponse<List<UserResponse>> readManagerByGrade(@PathVariable String categoryId,@PathVariable String grade){
+        List<UserResponse> list = this.userService.readManagerByGrade(Integer.parseInt(categoryId),Integer.parseInt(grade));
+        return new AsctApiResponse<>(list);
+    }
+
+    /* 총동문 임원 */
+    @RequestMapping(value = "/seniormanager/category/{categoryId}/grade/{grade}",method = RequestMethod.GET)
+    public AsctApiResponse<List<UserResponse>> readSeniorManager(@PathVariable String categoryId,@PathVariable String grade){
+        List<UserResponse> list = this.userService.readSeniorManager(Integer.parseInt(categoryId),Integer.parseInt(grade));
+        return new AsctApiResponse<>(list);
+    }
+
+
+
+
 }
--- association/src/main/java/me/skhu/service/UserService.java
+++ association/src/main/java/me/skhu/service/UserService.java
@@ -21,13 +21,39 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public void createUser(UserRequest userRequest){
+    public UserResponse createUser(UserRequest userRequest){
+        User user = new User();
+        user.setName(userRequest.getUser_name());
+        user.setLoginId(userRequest.getLogin_id());
+        user.setPassword(userRequest.getPassword());
 
+        userRepository.save(user);
+        return new UserResponse(user);
     }
 
     public List<UserResponse> readUserByCategoryId(int categoryId){
         List<User> list = this.userRepository.findByCategoryId(categoryId);
         return convertUserEntityToResponse(list);
+    }
+
+    public List<UserResponse> readUserByGrade(int categoryId,int grade){
+        List<User> list = this.userRepository.findByCategoryIdAndGrade(categoryId,grade);
+        return convertUserEntityToResponse(list);
+    }
+
+    public List<UserResponse> readManagerByGrade(int categoryId,int grade){
+        List<User> list = this.userRepository.findByCategoryIdAndGrade(categoryId,grade);
+        List<User> managers = Optional.ofNullable(list).orElse(Collections.emptyList()).stream()
+                .filter(user -> user.getPosition().getName().startsWith("기수"))
+                .collect(Collectors.toList());
+        return convertUserEntityToResponse(managers);
+    }
+    public List<UserResponse> readSeniorManager(int categoryId,int grade){
+        List<User> list = this.userRepository.findByCategoryIdAndGrade(categoryId,grade);
+        List<User> managers = Optional.ofNullable(list).orElse(Collections.emptyList()).stream()
+                .filter(user -> user.getPosition().getName().startsWith("총동문"))
+                .collect(Collectors.toList());
+        return convertUserEntityToResponse(managers);
     }
 
     private List<UserResponse> convertUserEntityToResponse(List<User> userList){
