ff14c3da66153c77a0ab5d4aeaaf9fa093200b71
aj1155 <aj1155@naver.com>
2017-01-24 13:26:49 +0900

    [api] baseEntity @CreatedDate 작동 위해 어노테이션 추가:

--- association/src/main/java/me/skhu/AssociationApplication.java
+++ association/src/main/java/me/skhu/AssociationApplication.java
@@ -3,7 +3,9 @@ package me.skhu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 @PropertySource({"classpath:DB.yml"})
 public class AssociationApplication {
